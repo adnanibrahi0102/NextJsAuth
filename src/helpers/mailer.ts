54d72cc4e04c8208038f42672ad9963c859065ed
Adnan ibrahim <adnanibrahi96@gmail.com>
2024-03-28 12:38:55 +0530

    completed signup route

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,6 +1,7 @@
 import User from "@/models/userModel";
 import nodemailer from "nodemailer";
 import bcryptjs from "bcryptjs";
+
 export const sendEmail = async ({ email, emailType, userId }: any) => {
   try {
     const hashedToken = await bcryptjs.hash(userId.toString(), 10);
@@ -10,20 +11,18 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
         verifyToken: hashedToken,
         verifyTokenExpiry: Date.now() + 3600000,
       });
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000,
+      });
     }
-    else if(emailType==="RESET"){
-        await User.findByIdAndUpdate(userId,{
-            forgotPasswordToken: hashedToken,
-            forgotPasswordTokenExpiry: Date.now() + 3600000,
-        });
-    }
-    const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false,
+    let transporter = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
       auth: {
-        user: "maddison53@ethereal.email",
-        pass: "jn7jnAPss4f63QBp6D",
+        user: "263c2544d425f5",
+        pass: "0939f439f7a127",
       },
     });
 
@@ -32,7 +31,15 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
       to: email,
       subject:
         emailType === "VERIFY" ? "Verify your email" : "Reset your password",
-      html: "<b>Hello world?</b>",
+      html: `<p>Click <a href="${
+        process.env.DOMAIN
+      }/verifyemail?token=${hashedToken}">here</a>to ${
+        emailType === "VERIFY" ? "verify your email" : "reset your password"
+      }
+      or copy and paste the link below in the browser.
+      <br>
+      ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+     </p>`,
     };
 
     const mailResponse = await transporter.sendMail(mailOptions);
